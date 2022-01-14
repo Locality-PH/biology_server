f7259fd0130c1851f79d4f991b3fd8f2a2ec6973
vonypeto <von.aralar@gmail.com>
2022-01-14 14:27:23 +0800

    netflify

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const fileUpload = require("express-fileupload")
+const fileUpload = require("express-fileupload");
+const serverless = require("serverless-http");
+
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-app.use(fileUpload())
+app.use(fileUpload());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const db = require("./app/models");
+const router = express.Router();
+app.use(`/.netlify/functions/api`, router);
+
 require("./app/auth");
 db.mongoose
   .connect(db.url, {
@@ -34,3 +39,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+module.exports.handler = serverless(app);
