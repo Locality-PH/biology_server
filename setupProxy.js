3b7aff0ba34ae33c276b5d9ca24f0c245776b363
vonypeto <von.aralar@gmail.com>
2022-01-14 18:32:19 +0800

    fixed

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
+    "/app",
     createProxyMiddleware({
       target: "https://xenodochial-gates-876873.netlify.app",
       changeOrigin: true,
