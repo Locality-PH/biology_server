faececa88cb9c50a76f356a504cdb1b59892ac88
vonypeto <von.aralar@gmail.com>
2022-01-14 11:14:30 +0800

    Merging process

--- app/controller/auth/accounts.controller.js
+++ app/controller/auth/accounts.controller.js
@@ -8,49 +8,64 @@ const Student = db.student;
 exports.registerUser = async (req, res) => {
   var account_id = new mongoose.Types.ObjectId();
   var teacher_id = new mongoose.Types.ObjectId();
-
+  const email = req.body.email;
   if (!req.body.email && req.body.uuid) {
     res.status(400).send({ message: "email or password can not be empty!" });
     return;
   }
 
-  console.log(account_id);
-  const users = new Account({
-    _id: account_id,
-    uuid: req.body.uuid,
-    email: req.body.email,
-    full_name: req.body.fullName,
-    role: "Admin",
-    teacher: teacher_id,
-  });
-  const teacher = new Teacher({
-    _id: teacher_id,
-  });
-  await users
-    .save(users)
-    .then(async (_) => {
-      await teacher
-        .save(teacher)
-        .then(async (_) => {
-          res.json({
-            role: "Admin",
-            mid: account_id,
-            tid: teacher_id,
-          });
-        })
-        .catch((err) => {
-          res.status(500).send({
-            message:
-              err.message || "Some error occurred while creating the Account.",
-          });
+  await Account.exists({ email: email }, async (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      currentUserId = result;
+      if (currentUserId !== null) {
+        // if user existed
+        console.log("user exist");
+        res.json("user already exist");
+      } else {
+        console.log(account_id);
+        const users = new Account({
+          _id: account_id,
+          uuid: req.body.uuid,
+          email: req.body.email,
+          full_name: req.body.fullName,
+          role: "Admin",
+          teacher: teacher_id,
         });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Account.",
-      });
-    });
+        const teacher = new Teacher({
+          _id: teacher_id,
+        });
+        await users
+          .save(users)
+          .then(async (_) => {
+            await teacher
+              .save(teacher)
+              .then(async (_) => {
+                res.json({
+                  role: "Admin",
+                  mid: account_id,
+                  tid: teacher_id,
+                });
+              })
+              .catch((err) => {
+                res.status(500).send({
+                  message:
+                    err.message ||
+                    "Some error occurred while creating the Account.",
+                });
+              });
+          })
+          .catch((err) => {
+            res.status(500).send({
+              message:
+                err.message ||
+                "Some error occurred while creating the Account.",
+            });
+          });
+      }
+    }
+  });
 };
 
 exports.loginUser = async (req, res) => {
