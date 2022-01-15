d28da8e7ea01b846d84da62222cbb77aec98528b
vonypeto <von.aralar@gmail.com>
2022-01-15 08:16:02 +0800

    last fixed

--- setupProxy.js
+++ setupProxy.js
@@ -1,30 +1,10 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//   app.use(
-//     "/.netlify/functions/api/",
-//     createProxyMiddleware({
-//       target: "https://xenodochial-gates-876873.netlify.app",
-//       changeOrigin: true,
-//     })
-//   );
-// };
-
-const proxy = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    proxy("/.netlify/functions/api/", {
-      target: "http://localhost:9000/",
-      pathRewrite: {
-        "^/\\.netlify/functions": "",
-      },
-    })
-  );
-
-  app.use(
-    "/.netlify/functions/api/",
-    proxy({
-      target: "https://xenodochial-gates-876873.netlify.app",
+    "/api",
+    createProxyMiddleware({
+      target: "https://xenodochial-gates-876873.netlify.app/",
       changeOrigin: true,
     })
   );
